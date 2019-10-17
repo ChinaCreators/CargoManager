62ed30ce1467fdf499dec97011968cd60efde974
creatorlxd <1461637345@qq.com>
2019-10-17 19:08:49 +0800

    fix a bug 2

--- source/ui.cpp
+++ source/ui.cpp
@@ -360,8 +360,10 @@ CargoView::CargoView(ShopManager& manager)
 			{
 				auto shop_name=m_pShop->currentText();
 				auto cargo_name=reinterpret_cast<Wt::WComboBox*>(list[i])->currentText();
-				auto cargo_change=std::stoi(reinterpret_cast<Wt::WLineEdit*>(list[i+1])->text().toUTF8());
-				if(!IsInvalidString(shop_name))
+				int cargo_change= 0;
+				if(!IsInvalidString(reinterpret_cast<Wt::WLineEdit*>(list[i+1])->text()))
+					cargo_change=std::stoi(reinterpret_cast<Wt::WLineEdit*>(list[i+1])->text().toUTF8());
+				if(!IsInvalidString(shop_name)&&!IsInvalidString(cargo_name))
 					m_ShopManager.m_Content[shop_name.toUTF8()].m_Content[cargo_name.toUTF8()].m_Size += cargo_change;
 			}
 			m_pContent->clear();
